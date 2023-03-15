7049b7a93d94efe58f0a96a76c31d3be5ab2f03a
AFURO-1130 <abcshotaro616@gmail.com>
2023-03-15 12:27:07 +0900

    fix link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 function Footer() {
   return (
     <footer className="bg-gray-800">
       <div className="max-w-7xl mx-auto flex flex-row justify-between py-2 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-xs font-bold text-white">金城翔太郎</h1>
+        <h1 className="text-xs font-bold text-white">
+          <Link href="https://kinjo.dev">金城翔太郎</Link>
+        </h1>
         <div className="flex flex-row justify-evenly">
           <a href="https://twitter.com/kinjyo1130" target="_blank" rel="noreferrer">
             <img src="/twitter.svg" alt="Twitter" className="w-6 h-6" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link';
+
 function Header() {
   return (
     <header className="bg-gray-800">
       <div className="max-w-7xl mx-auto flex flex-row justify-between py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-white">AI観光案内</h1>
-        <a className="text-white font-bold inline-block align-base" href="https://aitrip.canny.io/" target="_blank" rel="noreferrer">開発案募集</a>
+        <h1 className="text-3xl font-bold text-white">
+          <Link href="/">AI観光案内</Link>
+        </h1>
+        <a className="text-white font-bold inline-block align-base" href="https://aitrip.canny.io/" target="_blank" rel="noreferrer">
+          開発案募集
+        </a>
       </div>
     </header>
   );
